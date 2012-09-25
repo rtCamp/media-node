aea3ce56689ca62c0dfeba90836b7c6544c48bcb
MiteshShah <Mr.Miteshah@gmail.com>
2012-09-25 11:11:29 +0530

    Fix ubuntu.sh url

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ if [ $? -eq 0 ]
 then
 	echo -e "\033[34m Ubuntu Detected... \e[0m"
 	echo -e "\033[34m Fetching Ubuntu Installer Script... \e[0m"
-	curl https://github.com/rtCamp/media-node/blob/master/setup/ubuntu.sh | sudo bash
+	curl https://raw.github.com/rtCamp/media-node/master/setup/ubuntu.sh | sudo bash
 fi
