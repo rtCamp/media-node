f6c05a97741fe37b9d3f16f7a13792d9f9443081
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-01-28 22:00:24 +0530

    fix folder

--- api-rtcamp-com.js
+++ api-rtcamp-com.js
@@ -1,7 +1,13 @@
 var api = require('./lib/api.js')
 var queue = require('./lib/queue.js')
+var util = require('./util.js')
 
-// debugger;
+//config
+var env = process.env.NODE_ENV || "development"
+var config = require(__dirname + '/config.json')[env]
+
+//make sure upload folder exist
+util.makedir(config.folder);
 
 //fetch new jobs via API
 api.getjobs(function(res) {
