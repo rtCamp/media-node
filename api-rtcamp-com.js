e6537798910924941d1e67414a655389f6e3ccff
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-01-28 21:54:20 +0530

    intermediate

---
+++ api-rtcamp-com.js
@@ -0,0 +1,25 @@
+var api = require('./lib/api.js')
+var queue = require('./lib/queue.js')
+
+// debugger;
+
+//fetch new jobs via API
+api.getjobs(function(res) {
+    //process new jobs found in API
+    console.log("===================================================")
+    console.log("START processing queue")
+    console.log("===================================================")
+    queue.encode(res)
+    // queue.process(function(job) {
+    //     //update bandwidth
+    //     api.updatejob(job.api_job_id, 'bandwidth', job.bandwidth)
+    //     // update remote API
+    //     api.updatejob(job.api_job_id, 'duration', job.duration)
+    //     //update job status at API
+    //     api.updatejob(job.api_job_id, 'job_status', job.status)
+    //     //update out_file_location
+    //     api.updatejob(job.api_job_id, 'out_file_location', job.original_file_url)
+    //     //fire callback
+    //     api.callback(job)
+    // });
+})
